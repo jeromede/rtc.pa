8d5a3eaf0841bdecb3fb7445ed4c4db864587903
Jérôme D. <jeromede@fr.ibm.com>
2017-06-29 18:19:30 +0200

    Version with test program

---
+++ rtc.pa.connexion_test.plain/src/rtc/pa/read/DoIt.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2017 jeromede@fr.ibm.com
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+package rtc.pa.read;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.ibm.team.process.common.IProjectArea;
+import com.ibm.team.repository.client.IItemManager;
+import com.ibm.team.repository.client.ITeamRepository;
+import com.ibm.team.repository.common.IContributor;
+import com.ibm.team.repository.common.IContributorHandle;
+import com.ibm.team.repository.common.TeamRepositoryException;
+import com.ibm.team.workitem.client.IWorkItemClient;
+import com.ibm.team.workitem.common.IWorkItemCommon;
+import com.ibm.team.workitem.common.model.IWorkItemType;
+
+import rtc.utils.ProgressMonitor;
+
+public class DoIt {
+
+	public static String execute(ITeamRepository repo, IProjectArea pa, ProgressMonitor monitor)
+			throws TeamRepositoryException, IOException {
+
+		monitor.out(
+				"Successfully connected to project area " + pa.getName() + " (process: " + pa.getProcessName() + ")");
+		IWorkItemClient wiClient = (IWorkItemClient) repo.getClientLibrary(IWorkItemClient.class);
+		IWorkItemCommon wiCommon = (IWorkItemCommon) repo.getClientLibrary(IWorkItemCommon.class);
+		String result;
+		result = readMembers(repo, pa, monitor);
+		if (null != result)
+			return result;
+		result = readWorkItemTypes(repo, pa, wiClient, wiCommon, monitor);
+		if (null != result)
+			return result;
+		return null;
+	}
+
+	private static String readContributors(IContributorHandle[] contributors, ITeamRepository repo, IProjectArea pa,
+			ProgressMonitor monitor) {
+
+		IContributor contrib;
+		for (IContributorHandle contribHandle : contributors) {
+			try {
+				contrib = (IContributor) repo.itemManager().fetchCompleteItem(contribHandle, IItemManager.DEFAULT,
+						monitor);
+			} catch (TeamRepositoryException e) {
+				e.printStackTrace();
+				return "Error resolving IContributorHandle";
+			}
+			monitor.out("- " + contrib.getName() + " (" + contrib.getUserId() + ")");
+		}
+		return null;
+	}
+
+	private static String readMembers(ITeamRepository repo, IProjectArea pa, ProgressMonitor monitor) {
+
+		String result;
+		monitor.out("Its administrators are:");
+		result = readContributors(pa.getAdministrators(), repo, pa, monitor);
+		if (null != result)
+			return result;
+		monitor.out("Its members are:");
+		result = readContributors(pa.getMembers(), repo, pa, monitor);
+		if (null != result)
+			return result;
+		return null;
+	}
+
+	private static String readWorkItemTypes(ITeamRepository repo, IProjectArea pa, IWorkItemClient wiClient,
+			IWorkItemCommon wiCommon, ProgressMonitor monitor) {
+
+		monitor.out("Its work item types are:");
+		List<IWorkItemType> allWorkItemTypes;
+		try {
+			allWorkItemTypes = wiClient.findWorkItemTypes(pa, monitor);
+			for (IWorkItemType t : allWorkItemTypes) {
+				monitor.out("- " + t.getDisplayName() + " (" + t.getIdentifier() + ')');
+			}
+		} catch (TeamRepositoryException e) {
+			e.printStackTrace();
+			return "Problem while getting its work item types";
+		}
+		return null;
+	}
+
+}
